cba7c4abb9066d14c7773b28f33253d22e890b60
otrembskia <Arkadiusz.Otrembski@ts.fujitsu.com>
2016-11-18 13:25:50 +0100

    Added new enum products and extend products with names

--- src/main/java/model/EnrollEnums.java
+++ src/main/java/model/EnrollEnums.java
@@ -45,7 +45,14 @@ public class EnrollEnums {
     }
 
     public enum EGZAM_PRODUCT {
-        BASIC_ISTQB(0, "52"), BASIC_REQB(1, "55"), EXPERT_TEST_PROCESS(2, "53"), EXPERT_TEST_MANAGEMENT(3, "54");
+        BASIC_ISTQB(0, "52", "ISTQB Foundation Level"),
+        BASIC_REQB(1, "55", "REQB Foundation Level"),
+        EXPERT_TEST_PROCESS(2, "53","ISTQB Improving the Test Process"),
+        EXPERT_TEST_MANAGEMENT(3, "54","ISTQB Test Management"),
+        ADVANCED_TECHNICAL_TEST_ANALYST(4,"48","ISTQB Advanced Level Technical Test Analyst"),
+        ADVANCED_TEST_MANAGER(5,"49","ISTQB Advanced Level Test Manager"),
+        ADVANCED_TEST_ANALYST(6,"50","ISTQB Advancel Level Test Analyst"),
+        OTHER_AGILE_TESTER_EXTENTION(7,"51","ISTQB Agile Tester Extension");
 
         /**
          * EGZAM_PRODUCT data-original-index
@@ -56,9 +63,15 @@ public class EnrollEnums {
          */
         public final String deleteBtnId;
 
-        EGZAM_PRODUCT(int index, String deleteBtnId) {
+        /**
+         * EGZAM_PRODUCT name
+         */
+        public final String name;
+
+        EGZAM_PRODUCT(int index, String deleteBtnId, String name) {
             this.index = index;
             this.deleteBtnId = deleteBtnId;
+            this.name = name;
         }
     }
 }
