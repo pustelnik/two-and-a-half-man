d8c017f3160016ad4900fc166af2f80c5d868a87
jakubp <jakub.pustelnik@gmail.com>
2016-11-20 16:29:06 +0100

    - added id to Exam object

--- src/main/java/model/Exam.java
+++ src/main/java/model/Exam.java
@@ -3,15 +3,18 @@ package model;
 import model.EnrollEnums.EGZAM_LEVEL;
 import model.EnrollEnums.EGZAM_PRODUCT;
 
+import java.util.Optional;
+
 /**
  * @author jakubp on 20.11.16.
  * Container class for Exam in a meaning of /taahm/Session/Exams/ page
  */
 public class Exam {
-    private final EGZAM_LEVEL level;
     private final EGZAM_PRODUCT product;
     private final String numberOfSeats;
+    private final EGZAM_LEVEL level;
     private boolean registrationEnabled;
+    private Optional<Integer> id = Optional.empty();
 
     public Exam(
             EGZAM_LEVEL level,
@@ -40,5 +43,13 @@ public class Exam {
 
     public void setRegistrationEnabled(boolean registrationEnabled) {
         this.registrationEnabled = registrationEnabled;
+    }
+
+    public Optional<Integer> getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = Optional.of(id);
     }
 }
