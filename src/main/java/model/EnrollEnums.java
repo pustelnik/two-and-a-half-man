3115494f595ef4004535371a4ffbccc8432d652f
Jakub Pustelnik <jakub.pustelnik@ts.fujitsu.com>
2016-11-21 11:11:46 +0100

    fixed creating session by exam seat management method

--- src/main/java/model/EnrollEnums.java
+++ src/main/java/model/EnrollEnums.java
@@ -73,14 +73,14 @@ public class EnrollEnums {
     }
 
     public enum EGZAM_PRODUCT {
-        BASIC_ISTQB(0, "52", "ISTQB Foundation Level", BASIC),
-        BASIC_REQB(1, "55", "REQB Foundation Level", BASIC),
-        EXPERT_TEST_PROCESS(2, "53","ISTQB Improving the Test Process", EXPERT),
-        EXPERT_TEST_MANAGEMENT(3, "54","ISTQB Test Management", EXPERT),
-        ADVANCED_TECHNICAL_TEST_ANALYST(4,"48","ISTQB Advanced Level Technical Test Analyst", ADVANCE),
-        ADVANCED_TEST_MANAGER(5,"49","ISTQB Advanced Level Test Manager", ADVANCE),
-        ADVANCED_TEST_ANALYST(6,"50","ISTQB Advancel Level Test Analyst", ADVANCE),
-        OTHER_AGILE_TESTER_EXTENTION(7,"51","ISTQB Agile Tester Extension", OTHER);
+        BASIC_ISTQB(0, "114", "ISTQB Foundation Level", BASIC),
+        BASIC_REQB(1, "117", "REQB Foundation Level", BASIC),
+        EXPERT_TEST_PROCESS(2, "115","ISTQB Improving the Test Process", EXPERT),
+        EXPERT_TEST_MANAGEMENT(3, "116","ISTQB Test Management", EXPERT),
+        ADVANCED_TECHNICAL_TEST_ANALYST(4,"110","ISTQB Advanced Level Technical Test Analyst", ADVANCE),
+        ADVANCED_TEST_MANAGER(5,"112","ISTQB Advanced Level Test Manager", ADVANCE),
+        ADVANCED_TEST_ANALYST(6,"111","ISTQB Advancel Level Test Analyst", ADVANCE),
+        OTHER_AGILE_TESTER_EXTENTION(7,"113","ISTQB Agile Tester Extension", OTHER);
 
         /**
          * EGZAM_PRODUCT data-original-index
